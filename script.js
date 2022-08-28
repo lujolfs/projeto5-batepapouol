c6a0a6b8d2bdd6d429a6450a2b30fcf6298379f9
lujolfs <lucas.j.frank@gmail.com>
2022-08-27 21:04:24 -0300

    Implementação de envio e recebimento de mensagens.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//Perguntando e postando o nome.
-const nome = {name: prompt("Insira o seu nome!")};
-
+// Perguntando e postando o nome.
+const nick = prompt("Insira o seu nome!");
+const nome = {name: nick};
 const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome)
 
-//Funções verificando se o nome está disponível e possíveis erros.
+// Funções verificando se o nome está disponível e possíveis erros.
 requisicao.then(sucessoNome);
 requisicao.catch(erroNome);
 
 function sucessoNome(resposta) {
-    console.log("Nome show");
     console.log(resposta.data)
 }
 
@@ -17,27 +16,65 @@ function erroNome(erro) {
     console.log("Mensagem de erro: " + erro.response.data);
     if (erro.response.status === 400) {
         alert("Nome inválido. Por favor, escolha outro.");
-            //Não consegui pensar em uma forma de automatizar esse loop.
-            //Vou precisar achar outro jeito caso consiga fazer a página de login.
+            // Não consegui pensar em uma forma de automatizar esse loop.
+            // Vou precisar achar outro jeito caso consiga fazer a página de login.
         location.reload();
     }
 } 
 
-//Função para manter conexão
-
+// Função para manter conexão
 setInterval(statusConexao, 5000);
 function statusConexao() {
     const conexao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
-    console.log("Passaram 5 segundos.");
 }
 
-
-//Função para pegar os participantes do chat e possíveis erros.
+// Função para pegar os participantes do chat e possíveis erros.
 const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
 promessa.then(processarResposta);
 
 function processarResposta(resposta) {
     console.log(resposta.data)
-    console.log("Chegou familia")
 }
+
+// Função para receber as mensagens.
+let mensagens = []; // Cria array vazio para poder preenchê-lo com informações da API.
+const reqMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+reqMsg.then(mensagensChegaram);
+
+function mensagensChegaram(msgs) {
+    console.log(msgs.data)
+    mensagens = msgs.data;
+    renderizarMsgs();
+}
+
+// Função para enviar as mensagens.
+function capturaInput() {
+    var texto = document.getElementById('Mensagem').value;
+    const msgEnviada = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', {from: nick, to:"Todos", text:texto, type:"message"});
+}
+
+function envioMsg() {
+}
+
+// Função para renderizar as mensagens.
+function renderizarMsgs() {
+    const chat = document.querySelector('.conversa');
+    chat.innerHTML = '';
+
+    for(let i = 0; i < mensagens.length; i++) {
+        if (mensagens[i].type === "message") {
+            chat.innerHTML = chat.innerHTML + `<div class="mensagem-normal bubble"><div class="hora"><h6>${mensagens[i].time}</h6></div>
+            <p><span class="nome">${mensagens[i].from}</span> para <span class="nome">${mensagens[i].to}</span>: <span class="cont-men">${mensagens[i].text}</span></p>
+            </div>`
+        } else if (mensagens[i].type === "status") {
+            chat.innerHTML = chat.innerHTML + `<div class="status-entrada bubble"><div class="hora"><h6>${mensagens[i].time}</h6></div>
+            <p><span class="nome">${mensagens[i].from}</span> ${mensagens[i].text}</p>
+        </div>`
+        }
+        chat.lastElementChild.scrollIntoView();
+    }
+}
+
+// Função para atualizar as mensagens.
 
